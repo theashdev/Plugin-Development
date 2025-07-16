13ec693f7a735d0c5ae0ea8138a8959053c32470
Ash <ashopfiremc@gmail.com>
2025-07-16 00:55:06 +0000

    Initial commit

---
+++ Custom Books/Main.java
@@ -0,0 +1,12 @@
+package com.ash.testing;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getCommand("book").setExecutor(new BookCommand());
+    }
+}
