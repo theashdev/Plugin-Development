60b8b67b1adb940d8ed2af9cbaf1745e2f88d40d
Ash <ashopfiremc@gmail.com>
2025-07-16 07:21:50 +0000

    Initial commit

---
+++ Player Statistics/Main.java
@@ -0,0 +1,23 @@
+package com.ash.testing;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Statistic;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+
+        System.out.println(e.getPlayer().getStatistic(Statistic.DROP_COUNT));
+
+    }
+}
